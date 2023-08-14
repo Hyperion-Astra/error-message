389e2a8297a98ba073519c0d3d637ce3a581c771
hyperion-astra <eugeneemovon@gmail.com>
2023-08-14 12:08:50 +0100

    Linked the CSS

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let errorEl = document.getElementById ('error')
 function error(){
     errorEl.textContent = "Something went wrong, Please try again!"
 }
+errorEl.style.color = 'red'
+// errorEl.style.fontWeight = '200'
